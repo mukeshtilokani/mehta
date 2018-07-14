713bd4aca1473be083272fb9b20b2328618e665b
Sunny Sheth <ssheth@aecordigital.com>
2018-07-14 21:56:01 +0530

    jquery added

---
+++ public/backend/assets/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$(document).on('click', '.js-delete-button', function(e){
+        var action = $(this).data('delete-url');
+        var confirmationMsg = $(this).data('confirm-msg') || 'Are you sure?';
+        $(".js-delete-confirmation-msg").html(confirmationMsg);
+        $(".js-delete-modal-form").prop('action', action);
+    });
+});
