e26e8c9c02568e551827c27cb83a221be955bdfb
Mukesh Tilokani <mtilokani@aecordigital.com>
2018-07-15 20:04:06 +0530

    changes

---
+++ resources/views/pages/backend/galleries/edit.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.backend.default')
+
+@section('pageTitle', 'Admin')
+@section('plugins-style')
+@endsection
+
+@section('page-style')
+@endsection
+
+@section('content')
+	<h2 class="content-heading">Edit Gallery</h2>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="block">
+				<div class="block-content">
+					{!! Form::open(['route' => ['admin.galleries.update', $gallery->id], 'method' => 'PUT', 'class' => 'js-frm-edit-gallery', 'role' => 'form', 'enctype' => 'multipart/form-data']) !!}
+	    				@include('includes.backend.forms.gallery.form',['from'=>'edit'])
+					{{ Form::close() }}
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+
+@section('modal')
+@endsection
+
+@section('plugins-scripts')
+@endsection
+
+@section('page-scripts')
+	<script type="text/javascript" src="{{ asset('backend/assets/js/gallery.js') }}"></script>
+@endsection
---
+++ resources/views/pages/backend/galleries/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.backend.default')
+
+@section('pageTitle', 'Admin')
+
+@section('plugins-style')
+@endsection
+
+@section('page-style')
+@endsection
+
+@section('content')
+	<div class="block">
+		<div class="block-header block-header-default">
+			<h3 class="block-title">Galleries</h3>
+			<a class="btn sbold green" href="{{ route('admin.galleries.create') }}"> Add New
+                <i class="fa fa-plus"></i>
+            </a>
+		</div>
+		<div class="block-content">
+			<table class="table table-bordered table-striped table-vcenter js-dataTable-full">
+				<thead>
+					<tr>
+                        <th>Title</th>
+                        <th class="text-center" style="width: 15%;">Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($galleries as $gallery)
+					<tr>
+						<td class="font-w600">{{ $gallery->name }}</td>
+                        <td class="d-none d-sm-table-cell">{{ $gallery->parent ? $gallery->parent->name : '' }}</td>
+                        <td class="text-center">
+                        	<a href="galleries/{{ $gallery->id }}/edit" class="btn btn-icon-only green">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                            <a href="#" data-confirm-msg="Are you sure you would like to delete this record?" data-delete-url="galleries/{{ $gallery->id }}" class="btn btn-icon-only red js-delete-button" data-toggle="modal" data-target="#delete_modal"><i class="fa fa-trash"></i></a>
+                        </td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
+@endsection
+
+@section('modal')
+@endsection
+
+@section('plugins-scripts')
+@endsection
+
+@section('page-scripts')
+	<script type="text/javascript" src="{{ asset('backend/assets/js/gallery.js') }}"></script>
+@endsection
