316ba33253b7ca9d4e2dd0277ff2cc5b111d33f0
Parth <shahparth39@yahoo.com>
2018-07-24 00:05:25 +0530

    Revert "revert: gitignore file and pushing public files"
    This reverts commit 9dd0e04aa08858878fe6868699638ff8fb5c7184.

--- public/backend/assets/js/custom/common.js
+++
@@ -1,6 +0,0 @@
-/*!
- * business - v1.0.0
- * @author undefined - http://pixelperfection.in
- * Copyright (c) 2018
- */
-$(document).ready(function(){$(document).on("click",".js-delete-button",function(e){var t=$(this).data("delete-url"),o=$(this).data("confirm-msg")||"Are you sure?";$(".js-delete-confirmation-msg").html(o),$(".js-delete-modal-form").prop("action",t)}),$("#sortable").sortable({placeholder:"droppable"})});
--- public/backend/assets/js/custom/gallery.js
+++
@@ -1,6 +0,0 @@
-/*!
- * business - v1.0.0
- * @author undefined - http://pixelperfection.in
- * Copyright (c) 2018
- */
-var Gallery={init:function(){$(".js-frm-create-gallery, .js-frm-edit-gallery").validate({ignore:[],debug:!1,messages:{},rules:{title:{required:!0},gallery_image:{required:!0}},errorPlacement:function(e,i){"gallery_image"==i.prop("id")?i.closest(".js-image-div").append(e):i.parent().append(e)},submitHandler:function(e){e.submit()}}),$(document).on("change","#gallery_image",function(e){console.log("this.files",this.files),this.files&&this.files[0]&&$.each(this.files,function(){var e=new FileReader;e.onload=function(e){$(".js-preview-image").prop("src",e.target.result)},e.readAsDataURL(this)})})}};$(document).ready(function(){Gallery.init()});
